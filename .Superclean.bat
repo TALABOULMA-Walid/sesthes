546b5e810193ff2eff916bf5e93af1b4629a083a
TALABOULMA Walid <talaboulma.walid@gmail.com>
2018-09-27 15:19:35 +0200

    Adding clean scripts and gitignore from wtthesis

---
+++ .Superclean.bat
@@ -0,0 +1,8 @@
+REM Call a little clean
+CALL .clean.bat
+
+REM Cleaner Options:
+del Thesis.pdf
+del *.run.xml
+del *.synctex.gz
+
---
+++ .clean.bat
@@ -0,0 +1,29 @@
+REM Main directory clean
+del *.aux
+del *.bbl
+del *.bcf
+del *.blg
+del *.lof
+del *.log
+del *.maf
+del *.mtc*
+del *.out
+del *.sta
+del *.toc
+
+REM Subdirectories clean
+del .\text\*.aux
+del .\text\*.log
+
+REM Figures build clean
+del .\figures\*.aux
+del .\figures\*.bbl
+del .\figures\*.bcf
+del .\figures\*.blg
+del .\figures\*.lof
+del .\figures\*.log
+del .\figures\*.maf
+del .\figures\*.mtc*
+del .\figures\*.out
+del .\figures\*.sta
+del .\figures\*.toc
